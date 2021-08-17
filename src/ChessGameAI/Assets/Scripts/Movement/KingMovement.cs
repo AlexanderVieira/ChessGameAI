7d13df76899971f8e21e17781643bfc2535a4ddc
Alexander Silva <asilva943rj@gmail.com>
2021-08-17 01:25:08 -0300

    Implementando a morte do Rei, bem como o castling.

--- src/ChessGameAI/Assets/Scripts/Movement/KingMovement.cs
+++ src/ChessGameAI/Assets/Scripts/Movement/KingMovement.cs
@@ -3,6 +3,7 @@ using System.IO.MemoryMappedFiles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class KingMovement : Movement
 {
@@ -19,6 +20,52 @@ public class KingMovement : Movement
         moves.AddRange(UntilBlockedPath(new Vector2Int(-1, 1), true, 1));
         moves.AddRange(UntilBlockedPath(new Vector2Int(1, -1), true, 1));
         moves.AddRange(UntilBlockedPath(new Vector2Int(1, 1), true, 1));
+        moves.AddRange(Castling());
         return moves;
+    }
+
+    private List<Tile> Castling()
+    {
+        var moves = new List<Tile>();
+        if (Board._instance.SelectedPiece.GetComponent<King>().WasMoved)
+        {
+            return moves;
+        }
+        var tile = CheckRook(new Vector2Int(1, 0));
+        if (tile != null)
+        {
+            moves.Add(tile);
+        }
+        tile = CheckRook(new Vector2Int(-1, 0));
+        if (tile != null)
+        {
+            moves.Add(tile);
+        }
+        return moves;
+    }
+
+    private Tile CheckRook(Vector2Int direction)
+    {
+        Rook rook;
+        var currentTile = GetTile(Board._instance.SelectedPiece.tile.pos + direction);
+        while (currentTile != null)
+        {
+            if (currentTile.content != null)
+            {
+                break;
+            }
+            currentTile = GetTile(currentTile.pos + direction);
+        }
+
+        if (currentTile == null)
+        {
+            return null;
+        }
+        rook = currentTile.content as Rook;
+        if (rook == null || rook.WasMoved)
+        {
+            return null;
+        }
+        return rook.tile;
     }
 }
