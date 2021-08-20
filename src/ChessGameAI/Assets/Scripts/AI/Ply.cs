006e24debc33185f45cc18e46602d62d6294dcfa
Alexander Silva <asilva943rj@gmail.com>
2021-08-19 23:48:50 -0300

    Refatoração script PieceMovementState, bem como criação classe AffectedPiece..

--- src/ChessGameAI/Assets/Scripts/AI/Ply.cs
+++ src/ChessGameAI/Assets/Scripts/AI/Ply.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class Ply
 {
+    public string Name;
     public List<PieceEvaluation> Goldens;
     public List<PieceEvaluation> Greens;
+    public List<AffectedPiece> AffectedPieces;
     public float Score;
+    public MoveType MoveType;
+    public Ply OriginPly;
+    public List<Ply> FuturePlies;
+
 }
--- src/ChessGameAI/Assets/Scripts/StateMachine/States/PieceMovementState.cs
+++ src/ChessGameAI/Assets/Scripts/StateMachine/States/PieceMovementState.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using System.Data;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
@@ -9,40 +10,48 @@ using System;
 
 public class PieceMovementState : State
 {
+    public static List<AffectedPiece> AffectedPieces;
     public override async void EnterAsync()
     {
-        Debug.Log("Piece Movement State.");
-        var moveType = Board.Instance.SelectedHighlight.Tile.MoveType;
-        ClearEnPassant();
+        Debug.Log("Piece Movement State.");        
         var tcs = new TaskCompletionSource<bool>();
-        switch(moveType){
-
-            case MoveType.Normal:
-                NormalMove(tcs);
-                break;
-            case MoveType.Castling:
-                Castling(tcs);
-                break;
-            case MoveType.PawnDoubleMove:
-                PawnDoubleMove(tcs);
-                break;
-            case MoveType.EnPassant:
-                EnPassant(tcs);
-                break;
-            case MoveType.Promotion:
-                Promotion(tcs);
-                break;
-        }
-
+        ClearEnPassant();
+        MovePiece(tcs, false);        
         //await tcs.Task;
         await Task.Delay(100);
         Machine.ChangeTo<TurnEndState>();
     }
-    private async void Promotion(TaskCompletionSource<bool> tcs)
+
+    private void MovePiece(TaskCompletionSource<bool> tcs, bool skipMovement)
+    {
+        AffectedPieces = new List<AffectedPiece>();
+        var moveType = Board.Instance.SelectedHighlight.Tile.MoveType;
+        //ClearEnPassant();
+        switch(moveType){
+
+            case MoveType.Normal:
+                NormalMove(tcs, skipMovement);
+                break;
+            case MoveType.Castling:
+                Castling(tcs, skipMovement);
+                break;
+            case MoveType.PawnDoubleMove:
+                PawnDoubleMove(tcs, skipMovement);
+                break;
+            case MoveType.EnPassant:
+                EnPassant(tcs, skipMovement);
+                break;
+            case MoveType.Promotion:
+                Promotion(tcs, skipMovement);
+                break;
+        }
+    }
+
+    private async void Promotion(TaskCompletionSource<bool> tcs, bool skipMovement)
     {
         Debug.Log("Pawn Promotion");
         var movementTCS = new TaskCompletionSource<bool>();
-        NormalMove(movementTCS);
+        NormalMove(movementTCS, skipMovement);
         await movementTCS.Task;
         
         //NormalMove();
@@ -82,26 +91,27 @@ public class PieceMovementState : State
         }
     }
 
-    private void EnPassant(TaskCompletionSource<bool> tcs)
+    private void EnPassant(TaskCompletionSource<bool> tcs, bool skipMovement)
     {
         var pawn = Board.Instance.SelectedPiece;
         var direction = pawn.tile.pos.y > Board.Instance.SelectedHighlight.Tile.pos.y ? new Vector2Int(0, 1) : new Vector2Int(0, -1);
-        Debug.Log(Board.Instance.SelectedHighlight.Tile.pos + direction);
+        Debug.Log("En Passant:" + Board.Instance.SelectedHighlight.Tile.pos + direction);
         var enemy = Board.Instance.Tiles[Board.Instance.SelectedHighlight.Tile.pos + direction];
         enemy.content.gameObject.SetActive(false);
         enemy.content = null;
-        NormalMove(tcs);
+        NormalMove(tcs, skipMovement);
     }
 
-    private void PawnDoubleMove(TaskCompletionSource<bool> tcs)
+    private void PawnDoubleMove(TaskCompletionSource<bool> tcs, bool skipMovement)
     {
         var pawn = Board.Instance.SelectedPiece;
-        var direction = pawn.tile.pos.y > Board.Instance.SelectedHighlight.Tile.pos.y ? new Vector2Int(0, -1) : new Vector2Int(0, 1);
+        var direction = pawn.tile.pos.y > Board.Instance.SelectedHighlight.Tile.pos.y ? 
+                        new Vector2Int(0, -1) : new Vector2Int(0, 1);
         Board.Instance.Tiles[pawn.tile.pos + direction].MoveType = MoveType.EnPassant;
-        NormalMove(tcs);
+        NormalMove(tcs, skipMovement);
     }
 
-    private void Castling(TaskCompletionSource<bool> tcs)
+    private void Castling(TaskCompletionSource<bool> tcs, bool skipMovement)
     {
         var king = Board.Instance.SelectedPiece;
         king.tile.content = null;
@@ -136,28 +146,44 @@ public class PieceMovementState : State
         // LeanTween.move(rook.gameObject, new Vector3(rook.tile.pos.x, rook.tile.pos.y, 0), 1.4f);
     }
 
-    private void NormalMove(TaskCompletionSource<bool> tcs){
-
+    private void NormalMove(TaskCompletionSource<bool> tcs , bool skipMovement){
+        
         var piece = Board.Instance.SelectedPiece;
-        piece.transform.position = Board.Instance.SelectedHighlight.transform.position;
+        var pieceMoving = new AffectedPiece();
+        pieceMoving.Piece = piece;
+        pieceMoving.From = piece.tile;
+        AffectedPieces.Add(pieceMoving);
+        //piece.transform.position = Board.Instance.SelectedHighlight.transform.position;
         piece.tile.content = null;
         piece.tile = Board.Instance.SelectedHighlight.Tile;
         if (piece.tile.content != null)
         {
-            var deadPiece = piece.tile.content;
+            var deadPiece = piece.tile.content as Piece;
+            var pieceKilled = new AffectedPiece();
+            pieceKilled.Piece = deadPiece;
+            pieceKilled.From = piece.tile;
+            AffectedPieces.Add(pieceKilled);
             Debug.LogFormat("The Piece {0} has been captured", deadPiece.transform.name);
             deadPiece.gameObject.SetActive(false);
         }
         piece.tile.content = piece;
         piece.WasMoved = true;
-        tcs.SetResult(true);
 
-        //var timing = Vector3.Distance(piece.transform.position, Board.Instance.SelectedHighlight.transform.position) * 0.5f;
-        // LeanTween.move(piece.gameObject, Board.Instance.SelectedHighlight.transform.position, timing)
-        //     .setOnComplete(() => 
-        //     { 
-        //         tcs.SetResult(true); 
-        //     }); 
+        if (!skipMovement)
+        {
+            piece.transform.position = Board.Instance.SelectedHighlight.transform.position;
+            tcs.SetResult(true);
+        }else
+        {
+            //var timing = Vector3.Distance(piece.transform.position, Board.Instance.SelectedHighlight.transform.position) * 0.5f;
+            // LeanTween.move(piece.gameObject, Board.Instance.SelectedHighlight.transform.position, timing)
+            //     .setOnComplete(() => 
+            //     { 
+            //         tcs.SetResult(true); 
+            //     });
+            tcs.SetResult(true);
+        }
+                 
     }
    
 }
